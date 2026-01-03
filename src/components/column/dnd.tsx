d96a09a5f106173e3e97675d6b3d5f4e9560ee12
Spicy - musicenter <zerikarliodam@gmail.com>
2026-01-04 00:57:48 +0500

    Update dnd.tsx

--- src/components/column/dnd.tsx
+++ src/components/column/dnd.tsx
@@ -87,7 +87,7 @@ export function Dnd() {
       </OverlayScrollbar>
       {isMobile && (
         <div className="flex justify-center">
-          <span className="text-sm text-gray-500 text-center">左右滑动查看更多</span>
+          <span className="text-sm text-gray-500 text-center">Batafsil ma'lumot olish uchun chapga yoki o'ngga suring</span>
         </div>
       )}
     </DndWrapper>
